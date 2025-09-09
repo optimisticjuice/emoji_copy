ab895738d6948b2fcfeb5573f1190549ad3bc234
Justamanuelax <justamanuelax@gmail.com>
2025-09-09 19:22:13 +0200

    Error Fixed + JSX(HTML) skeleton done

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,28 @@
 import './Style.css';
 const API_KEY = import.meta.env.VITE_API_KEY;
 const API_URL = 'https://api.api-ninjas.com/v1/emoji?name=';
-function App() {
-   const [input, setInput ] = useState('');
-   const [emojiStory, setEmojiStory] = useState([]);
-   const [emojiNames, setEmojiNames] = useState([]);
-   const [loading, setLoading] = useState(false);
-   const [error, setError] = useState('');
-   const [searchHistory, setSearchHistory] = useState([]);
-  
 
-   
+function App() {
    return (
     <>
+    <div className="App" style={{ maxWidth: '600px', margin: '0 auto', padding: '20px' }}>
+      <h1>📖 Emoji Story</h1>
+      <div>
+        <input type="text" />
+      </div>
+      {/* {} */}
+      {/* {} */}
+    <div>
+      <div>
+        {/*         {emojiStory.map((emoji, index) => (
+        */}
+      </div>
+    </div>
+    <div>
+      <p>Tip : Type the word first and press space to turn it into an emoji.</p>
+    </div>
+    </div>
+    
 
     </>
   )
