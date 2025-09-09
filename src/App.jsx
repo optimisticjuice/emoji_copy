c902a49884bbdb5923881bc97bada43be982ae85
Justamanuelax <justamanuelax@gmail.com>
2025-09-09 19:52:39 +0200

    3. Setting up toggleTinyText on the case of a doubleclick on line 50 div

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import './Style.css';
 // const API_KEY = import.meta.env.VITE_API_KEY;
 // const API_URL = 'https://api.api-ninjas.com/v1/emoji?name=';
-
-
-
+// TODO: uncomment the APIs when the API is ready
 
 // useState hook needs to be inside the App function
 function App() {
   const [input, setInput] = useState('');
-
+  const [isTinyTextDisplayed, setIsTinyTextDisplayed] = useState(false);
   const handleInputChange = (event) => {
   // handleInputChange function changes the value of the input so what is typed is stored in the input variable
     setInput(event.target.value);
   }
+//  The toggleTinyText togglese the isTinyTextDisplayed state as a boolean from false to true and vice versa looped
+  const toggleTinyText = () => {
+    setIsTinyTextDisplayed(prev => !prev);
+  };
+
+  useEffect(() => {
+    const tinytextElement = document.querySelectorAll('.tinytext');
+    //  querySelectorAll is for all the class elements with the name tinytext
+    // checks if the tinyTextElement exists then runs the code inside the if statement
+    if (tinytextElement) {
+      // loops through the tinytextElement
+      tinytextElement.forEach(element => {
+        // Displays the tinytext element if isTinyTextDisplayed is true and hides it if it is false
+        element.style.display = isTinyTextDisplayed ? 'block' : 'none';
+      });
+    }
+    // changes the code inside the useEffect function when the isTinyTextDisplayed state changes
+  }, [isTinyTextDisplayed]);
+
   return (
     <>
     <div className="App" style={{ maxWidth: '600px', margin: '0 auto', padding: '20px' }}>
@@ -30,8 +47,8 @@ function App() {
       </div>
       {/* {} */}
       {/* {} */}
-    <div>
-      <div>
+    <div onDoubleClick={toggleTinyText}>
+      <div className='rowlike'>
         {/*         {emojiStory.map((emoji, index) => (
         */}
       </div>
